a2b116d701317e6cd0f1858dd19f227ecca24c44
Tracey-goode <Jerand322@gmail.com>
2025-08-06 21:19:11 -0400

    second commit

--- Assignment.js
+++ Assignment.js
@@ -7,39 +7,70 @@
 
 // // Part 1
 
-const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26"
+// const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26"
 
-const csvArray = csvData.split(",")
-console.log(csvArray)
+// const csvArray = csvData.split(",")
+// console.log(csvArray)
 
-let cell1 = ""; cell2 = ""; cell3 = ""; cell4 = "";
+// let cell1 = ""; cell2 = ""; cell3 = ""; cell4 = "";
 
-let commas = 0
+// let commas = 0
 
-for (let i = 0; i < csvData.length; i++) {
-    if(csvData[i] ==","){
-    }else if(csvData[i]=='\n'){
-        console.log(cell1, cell2, cell3, cell4)
+// for (let i = 0; i < csvData.length; i++) {
+//     if(csvData[i] ==","){
+//     }else if(csvData[i]=='\n'){
+//         console.log(cell1, cell2, cell3, cell4)
         
-        cell1 = cell2 = cell3 = cell4 = ""
-        commas = 0
+//         cell1 = cell2 = cell3 = cell4 = ""
+//         commas = 0
 
-    } else{
-        if(commas == 0){
-            cell1 +=csvData[i];
-        } else if (commas == 1){
-            cell2 +=csvData[i];
-        } else if (commas == 2){
-            cell3 += csvData[i];
-        } else{
-            cell4 += csvData[i];
-        }
-    }
-  if(csvData.length - 1 == i){
-    console.log(cell1, cell2, cell3, cell4)
-}  
-}
+//     } else{
+//         if(commas == 0){
+//             cell1 +=csvData[i];
+//         } else if (commas == 1){
+//             cell2 +=csvData[i];
+//         } else if (commas == 2){
+//             cell3 += csvData[i];
+//         } else{
+//             cell4 += csvData[i];
+//         }
+//     }
+//   if(csvData.length - 1 == i){
+//     console.log(cell1, cell2, cell3, cell4)
+// }  
+// }
 
 // // Part 2
 // Declare a variable that stores the number of columns in each row of data within the csv
-// Instead of hardcoding four columns per row, expand your code to accpet any number of columns
+// Instead of hardcoding four columns per row, expand your code to accept any number of columns.
+
+
+const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26"
+
+const rows = csvData.split('\n')
+
+const headers = rows[0].split(',');
+const numColumns = headers.length;
+
+const data = []
+
+for( let row of rows){
+    const cells = [];
+    let currentCell = '';
+    let commaCount = 0;
+
+    for (let i = 0; i < row.length; i++){
+        const char= row[i];
+        if (char === ','){
+            cells.push(currentCell);
+            currentCell = '';
+            commaCount++;
+        } else{
+            currentCell += char;
+        }
+    }
+    cells.push(currentCell);
+
+    data.push(cells)
+}
+console.log(data)
